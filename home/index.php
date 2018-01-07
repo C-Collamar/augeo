6ef500c486fe32726bce46ddc79f42859405e5fa
C-Collamar <christian.collamar@gmail.com>
2018-01-07 23:30:56 +0800

    Add initial files and bootstrap submodule

---
+++ home/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	<link rel="stylesheet" href="../global/css/topbar.css">
+</head>
+<body>
+	<nav id="topbar">
+		<ul>
+			<li>
+				<div id="main-logo"></div>
+			</li>
+		</ul>
+	</nav>
+</body>
+</html>
