362c642063889a36e30b2650bf424a6c4475b15a
C-Collamar <christian.collamar@gmail.com>
2018-01-08 20:56:55 +0800

    Add topbar

--- home/index.php
+++ home/index.php
@@ -2,16 +2,13 @@
 <html>
 <head>
 	<title>Home</title>
-	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="../global/css/topbar.css">
+	<link rel="stylesheet" href="../global/vendor/bootstrap/dist/css/bootstrap.min.css">
+	<script src="../global/vendor/jquery/dist/jquery.min.js"></script>
+	<script src="../global/vendor/bootstrap/dist/js/bootstrap.min.js"></script>
 </head>
 <body>
-	<nav id="topbar">
-		<ul>
-			<li>
-				<div id="main-logo"></div>
-			</li>
-		</ul>
-	</nav>
+	<?php require "../global/php/topbar.html" ?>
 </body>
 </html>
