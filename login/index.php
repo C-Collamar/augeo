c7107e5558c61dcf7eaa5920e6bd4e37d20ac9f7
Earl Austin Avila Zuniga <earlaustinavilazuniga@gmail.com>
2018-01-10 14:41:30 +0700

    update login page

--- login/index.php
+++ login/index.php
@@ -11,25 +11,35 @@
 <link rel="stylesheet" href="../global/vendor/bootstrap/dist/css/bootstrap.min.css">
 
 <body>
-<form  method="post" enctype="multipart/form-data">
-<div id="login_form" class="login_form">
 
+<div id="login_form" class="login_form">
+<form  method="post" enctype="multipart/form-data">
     <div id ="error_msg" class="error_msg">
 <p>  </p><br>
 </div>
-
-
+<!-- Login form -->
+ <h1>Login to AUGEO</h1>
 <input type="text" name="uname" id="uname" placeholder="Enter Username" required><br>
-
-
 <input type="password" name="pass" id="pass" placeholder="Enter Password" required><br>
-
 <input type="button" name="submit" id="submit" value="Login">
 <br>
 
 <div id="forgot_pass" class="forgot_pass"><a href="Password_reset/"> Forgot Password</a></div>
+<a href="#" class="show_hide">Sign Up</a><br />
 </form>
 </div>
+
+<!-- Create form -->
+<div id="crt_form" class="crt_form">
+    <h1>Create a New Account</h1>
+    <input type="text" name="crt_uname" id="crt_uname" placeholder="Username" required><br>
+    <input type="password" name="crt_pass" id="crt_pass" placeholder="Password" required><br>
+    <input type="button" name="crt_acc" id="crt_acc" value="Create account"><br><br>
+    <a href="#" class="show_hide">Already have an account? Log in</a><br />
+
+</div>
+
+
  <script src="../global/vendor/jquery/dist/jquery.min.js"></script>
     <script src="../global/vendor/bootstrap/dist/js/bootstrap.min.js"></script>
 <script src="js/index.js"></script>
--- login/js/index.js
+++ login/js/index.js
@@ -1,3 +1,4 @@
+//request
 $(document).ready(function() {
              $("#submit").click(function(){
                 $.ajax({
@@ -8,9 +9,25 @@ $(document).ready(function() {
                         pass : $("#pass").val()
                     },
                     success: function(result) {
-                        // show returned message in a pop up
+                        // show returned message
                         document.getElementById("error_msg").innerHTML= result;
                     }
                });
           });
      });
+
+// hiding and showing login form/Create form
+$(document).ready(function(){
+
+
+  $(".crt_form").hide();
+  $(".show_hide").show();
+  $(".crt_form").hide();
+
+  $('.show_hide').click(function(){
+  $(".crt_form").slideToggle();
+  $(".login_form").slideToggle();
+
+  });
+
+});
