c8746cbe44dc9c1b27a5da3e3a06c0069674194a
Earl Austin Avila Zuniga <earlaustinavilazuniga@gmail.com>
2018-01-11 18:30:09 +0700

    finished login page

--- home/index.php
+++ home/index.php
@@ -8,7 +8,7 @@
 	<link rel="stylesheet" href="css/home.css">
 </head>
 <body>
-	<?php require "../global/php/topbar.html" ?>
+	<?php require "../global/php/topbar.html"; ?>
 
 	<div class="container-fluid">
 		<div class="row">
