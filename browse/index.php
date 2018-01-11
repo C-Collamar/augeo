9cb440e9f1ef85578db2ed176ae18a6156ad1494
C-Collamar <christian.collamar@gmail.com>
2018-01-12 00:13:57 +0800

    Finish side-panel

--- browse/index.php
+++ browse/index.php
@@ -11,32 +11,30 @@
 <body>
 	<?php require "../global/php/topbar.html" ?>
 
-	<div style="position: relative; border: 1px solid black">
-		<div id="browse-content">
-			<button class="btn btn-default" onclick="toggleOptionPanel()">Options</button>
+	<div id="browse-content">
+		<button class="btn btn-default" onclick="toggleOptionPanel()">Options</button>
+	</div>
+	<div id="side-panel">
+		<div style="margin-bottom: 10px;">
+			<button class="btn btn-default" style="width: 100%;" onclick="toggleOptionPanel()">CLOSE</button>
 		</div>
-		<div id="side-panel">
-			<div style="margin-bottom: 10px;">
-				<button class="btn btn-default" style="width: 100%;" onclick="toggleOptionPanel()">CLOSE</button>
-			</div>
-			<div class="panel panel-default">
-				<div class="panel-heading">Search</div>
-				<div class="panel-body">
-					<div style="margin-bottom: 10px;">
-						<label class="radio-inline"><input type="radio" name="optradio" checked name="search-by">Item name</label>
-						<label class="radio-inline"><input type="radio" name="optradio" name="search-by">Item description</label>
-					</div>
-					<div class="input-group">
-						<input type="text" class="form-control" placeholder="Search">
-						<div class="input-group-btn">
-							<button class="btn btn-default" type="submit">
-								<i class="glyphicon glyphicon-search"></i>
-							</button>
-						</div>
+		<div class="panel panel-default">
+			<div class="panel-heading">Search</div>
+			<div class="panel-body">
+				<div style="margin-bottom: 10px;">
+					<label class="radio-inline"><input type="radio" name="optradio" checked name="search-by">Item name</label>
+					<label class="radio-inline"><input type="radio" name="optradio" name="search-by">Item description</label>
+				</div>
+				<div class="input-group">
+					<input type="text" class="form-control" placeholder="Search">
+					<div class="input-group-btn">
+						<button class="btn btn-default" type="submit">
+							<i class="glyphicon glyphicon-search"></i>
+						</button>
 					</div>
 				</div>
 			</div>
-		</div>s
+		</div>
 	</div>
 
 	<script src="../global/vendor/jquery/dist/jquery.min.js"></script>
