30a822371b6c929d68c0349e6e27806972016933
C-Collamar <christian.collamar@gmail.com>
2018-01-12 21:28:06 +0800

    Add item cards

--- browse/index.php
+++ browse/index.php
@@ -11,43 +11,58 @@
 <body>
 	<?php require "../global/php/topbar.html" ?>
 
+	<button class="btn btn-default" onclick="toggleOptionPanel()">Options</button>
 	<div id="browse-content">
-		<button class="btn btn-default" onclick="toggleOptionPanel()">Options</button>
+		<div class="container">
+			<div class="card">
+				<img src="http://localhost/augeo/data/user/items/0_0.jpg" alt="">
+				<div class="container-fluid">
+					<h4><b>XBox360</b></h4> 
+					<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus, sapiente...</p> 
+					<div class="highest-bid">Php 5.00</div>
+					<div class="tag-list">
+						<a href="">tagname-2</a>
+						<a href="">tagname-1</a>
+						<a href="">tagname-3</a>
+					</div>
+				</div>
+			</div>
+			<div class="card">
+				<img src="http://localhost/augeo/data/user/profile_img/0.png" alt="">
+				<div class="container-fluid">
+					<h4><b>Item name</b></h4> 
+					<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus, sapiente...</p> 
+				</div>
+			</div>
+		</div>
 	</div>
 	<div id="side-panel">
 		<div style="margin-bottom: 10px;">
-			<button class="btn btn-default" style="width: 100%;" onclick="toggleOptionPanel()">CLOSE</button>
+			<button class="btn btn-default" onclick="toggleOptionPanel()">Close</button>
 		</div>
 		<div class="panel panel-default">
 			<div class="panel-heading">Search</div>
 			<div class="panel-body">
 				<div style="margin-bottom: 10px;">
-					<label class="radio-inline"><input type="radio" name="optradio" checked name="search-by">Item name</label>
-					<label class="radio-inline"><input type="radio" name="optradio" name="search-by">Tag name</label>
+					<label class="radio-inline"><input type="radio" name="" name="" checked>Item name</label>
+					<label class="radio-inline"><input type="radio" name="" name="">Tag name</label>
 				</div>
-				<div class="input-group">
-					<input type="text" class="form-control" placeholder="Search">
-					<div class="input-group-btn">
-						<button class="btn btn-default" type="submit">
-							<i class="glyphicon glyphicon-search"></i>
-						</button>
-					</div>
-				</div>
+				<input type="text" class="form-control" placeholder="Search term">
 			</div>
 		</div>
 		<div class="panel panel-default">
 			<div class="panel-heading">Sort</div>
 			<div class="panel-body">
-				<div>
-					<div class="checkbox">
-						<label><input type="checkbox" value="">Reverse order</label>
-					</div>
+				<div id="ordering-options">
+					<label class="radio-inline"><input type="radio" name="" checked>Ascending</label>
+					<label class="radio-inline"><input type="radio" name="">Descending</label>
 				</div>
 				<hr style="margin: 10px 0px 10px 0px;">
-				<div><label class="radio-inline"><input type="radio" name="optradio" checked name="search-by">Item name</label></div>
-				<div><label class="radio-inline"><input type="radio" name="optradio" name="search-by"></label></div>
+				<div><label class="radio-inline"><input type="radio" name="" checked>Item name</label></div>
+				<div><label class="radio-inline"><input type="radio" name="">Date of upload</label></div>
 			</div>
 		</div>
+		<center><button class="btn btn-green" onclick="toggleOptionPanel()">Search</button></center>
 	</div>
 
 	<script src="http://localhost/augeo/global/vendor/jquery/dist/jquery.min.js"></script>
