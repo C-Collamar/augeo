a60204a6a73d4d10eb942e38756efc25c5c0f661
C-Collamar <christian.collamar@gmail.com>
2018-01-13 23:03:39 +0800

    Additional fixes

--- browse/js/browse.js
+++ browse/js/browse.js
@@ -3,7 +3,6 @@ $("#browse_nav").addClass("active");
 var $grid = $(".grid").masonry({
 	itemSelector: ".grid-item",
 	columnWidth: ".grid-sizer",
-	// gutter: 10,
 	percentPosition: true,
 	horizontalOrder: true
 });
@@ -12,9 +11,3 @@ function toggleOptionPanel() {
 	$("#browse-content").toggleClass("expand");
 	$("#side-panel").toggleClass("contract");
 }
-
-// $(window).resize(function(event) {
-// 	grid.masonry("reload");
-// })
-
-console.log(document.getElementsByClassName("grid-sizer")[0].offsetWidth);
