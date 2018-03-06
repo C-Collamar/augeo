32b83699cf8613d399ced7f97e1a5f31db203c28
C-Collamar <christian.collamar@gmail.com>
2018-03-06 20:05:36 +0800

    Minor improvements

--- item/view/index.php
+++ item/view/index.php
@@ -4,6 +4,33 @@ require $_SERVER['DOCUMENT_ROOT']."/augeo/global/php/session.php";
 require $_SERVER['DOCUMENT_ROOT']."/augeo/global/php/connection.php";
 require $_SERVER['DOCUMENT_ROOT']."/augeo/global/php/encrypt.php";
 
+function display404() {
+    exit(
+    '<style>'.
+        '#back-btn {'.
+            'display: block;'.
+            'width: fit-content;'.
+            'margin: 30px auto 0px auto;'.
+            'background-color: #a53a41;'.
+            'border: 4px solid #a53a41;'.
+            'padding: 10px 16px;'.
+            'color: white;'.
+            'transition: background-color, color 0.2s;'.
+        '}'.
+        '#back-btn:hover {'.
+            'background-color: #ffffff;'.
+            'color: #a53a41;'.
+        '}'.
+    '</style>'.
+    '<div style="text-align: center; padding-top: 50px;">'.
+        '<div style="font-size: 100px; user-select: none">404</div>'.
+        '<div style="font-size: 20px; ">OOPS! SORRY WE CAN\'T FIND THAT ITEM!</div>'.
+        '<div style="font-size: 15px; ">How about visiting our browsing section?</div>'.
+        '<a href="http://localhost/augeo/browse" id="back-btn">Have fun</a>'.
+    '</div>'
+    );
+}
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -16,34 +43,8 @@ require $_SERVER['DOCUMENT_ROOT']."/augeo/global/php/encrypt.php";
     <link rel="stylesheet" href="css/view_item.css">
 </head>
 <body>
-    <?php require $_SERVER['DOCUMENT_ROOT']."/augeo/global/php/topbar.php"; ?>
     <?php
-    function display404() {
-        exit(
-        '<style>'.
-            '#back-btn {'.
-                'display: block;'.
-                'width: fit-content;'.
-                'margin: 30px auto 0px auto;'.
-                'background-color: #a53a41;'.
-                'border: 4px solid #a53a41;'.
-                'padding: 10px 16px;'.
-                'color: white;'.
-                'transition: background-color, color 0.2s;'.
-            '}'.
-            '#back-btn:hover {'.
-                'background-color: #ffffff;'.
-                'color: #a53a41;'.
-            '}'.
-        '</style>'.
-        '<div style="text-align: center; padding-top: 50px;">'.
-            '<div style="font-size: 100px; user-select: none">404</div>'.
-            '<div style="font-size: 20px; ">OOPS! SORRY WE CAN\'T FIND THAT ITEM!</div>'.
-            '<div style="font-size: 15px; ">How about visiting our browsing section?</div>'.
-            '<a href="http://localhost/augeo/browse" id="back-btn">Have fun</a>'.
-        '</div>'
-        );
-    }
+    require $_SERVER['DOCUMENT_ROOT']."/augeo/global/php/topbar.php";
 
     if(isset($_GET['id'])) { //if ID is present in the URI
         $item_id = encode($_GET['id']);
@@ -69,6 +70,9 @@ require $_SERVER['DOCUMENT_ROOT']."/augeo/global/php/encrypt.php";
         if(mysqli_num_rows($item_info) <= 0) {
             display404();
         }
+        else {
+            $item = $item_info->fetch_assoc();
+        }
 
         //get item tag(s)
         $query =
@@ -89,21 +93,29 @@ require $_SERVER['DOCUMENT_ROOT']."/augeo/global/php/encrypt.php";
     ?>
     <div class="container-fluid" style="padding: 20px 0px 0px 0px">
         <div class="col-sm-9">
-            <div id="title">Super amazing title</div>
-            <div id="description" class="well">
-                A home 250GB slim game console developed by Microsoft. As the successor to the original Xbox, it is the second console in the Xbox series nd it's super fun to play with. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusamus, sapiente.
-            </div>
-            <div id="item-images" class="well">
-                <img src="http://localhost/augeo/data/user/items/17_1.jpg">
-                <img src="http://localhost/augeo/data/user/items/17_0.jpg">
-            </div>
-            <div class="tag-list">
-                <a href="">tagname-2</a>
-                <a href="">tagname-1</a>
-                <a href="">tagname-3</a>
+            <div class="well">
+                <div id="title"><?php echo decode($item['name']) ?></div>
+                <hr>
+                <div id="description"><?php echo decode($item['description']) ?></div>
+                <hr>
+                <div id="item-images">
+                    <?php
+                    while($path = $img_paths->fetch_assoc()) {
+                        echo '<img src="'.decode($path['img_path']).'">';
+                    }
+                    ?>
+                </div>
+                <hr>
+                <div class="tag-list">
+                    <?php
+                    while($tag = $tags->fetch_assoc()) {
+                        echo '<a href="">'.decode($tag['tag_name']).'</a>';
+                    }
+                    ?>
+                </div>
             </div>
         </div>
-        <div class="col-sm-3">Right side</div>
+        <div class="col-sm-3">Related Items</div>
     </div>
     <?php } else display404(); ?>
 
