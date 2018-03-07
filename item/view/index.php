1310f1d20c812824e6441871067dc098f75db7bb
C-Collamar <christian.collamar@gmail.com>
2018-03-08 01:38:10 +0800

    Redesign item viewiing

--- item/view/index.php
+++ item/view/index.php
@@ -15,7 +15,7 @@ function display404() {
             'border: 4px solid #a53a41;'.
             'padding: 10px 16px;'.
             'color: white;'.
-            'transition: background-color, color 0.2s;'.
+            'transition: 0.2s;'.
         '}'.
         '#back-btn:hover {'.
             'background-color: #ffffff;'.
@@ -26,7 +26,7 @@ function display404() {
         '<div style="font-size: 100px; user-select: none">404</div>'.
         '<div style="font-size: 20px; ">OOPS! SORRY WE CAN\'T FIND THAT ITEM!</div>'.
         '<div style="font-size: 15px; ">How about visiting our browsing section?</div>'.
-        '<a href="http://localhost/augeo/browse" id="back-btn">Have fun</a>'.
+        '<a href="http://localhost/augeo/browse" id="back-btn">Have fun!</a>'.
     '</div>'
     );
 }
@@ -91,35 +91,125 @@ function display404() {
         $query = "SELECT img_path FROM augeo_user_end.item_img WHERE item_id = $item_id";
         $img_paths = $conn->query($query) or die('ERROR: '.mysqli_error($conn).'<br>'.'QUERY: '.$query);
     ?>
-    <div class="container-fluid" style="padding: 20px 0px 0px 0px">
-        <div class="col-sm-9">
-            <div class="well">
-                <div id="title"><?php echo decode($item['name']) ?></div>
-                <hr>
-                <div id="description"><?php echo decode($item['description']) ?></div>
-                <hr>
-                <div id="item-images">
-                    <?php
-                    while($path = $img_paths->fetch_assoc()) {
-                        echo '<img src="'.decode($path['img_path']).'">';
-                    }
-                    ?>
+    <div class="container" style="padding-top: 20px">
+        <div class="row">
+            <div class="col-sm-5">
+                <div id="imgCarousel" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#imgCarousel" data-slide-to="0" class="active"></li>
+                        <li data-target="#imgCarousel" data-slide-to="1"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="item active">
+                            <img src="http://localhost/augeo/data/user/items/17_0.jpg">
+                        </div>
+                        <div class="item">
+                            <img src="http://localhost/augeo/data/user/items/19_0.jpg">
+                        </div>
+                    </div>
+                    <!-- Left and right controls -->
+                    <a class="left carousel-control" href="#imgCarousel" data-slide="prev">
+                        <span class="glyphicon glyphicon-chevron-left"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="right carousel-control" href="#imgCarousel" data-slide="next">
+                        <span class="glyphicon glyphicon-chevron-right"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
                 </div>
-                <hr>
-                <div class="tag-list">
-                    <?php
-                    while($tag = $tags->fetch_assoc()) {
-                        echo '<a href="">'.decode($tag['tag_name']).'</a>';
-                    }
-                    ?>
+            </div>
+            <div class="col-sm-7">
+                <div id="title">Sample Item Name</div>
+                <div style="text-align: center">
+                    <div style="display: inline-block">
+                        <span id="amount-label">Current amount</span><span id="amount">Php 275.00</span>
+                    </div>
+                    <div style="display: inline-block">
+                        <button id="bid-sect-toggler" class="btn btn-success" data-toggle="collapse" data-target="#bid-sect">Bid</button>
+                    </div>
+                </div>
+                <div id="bid-sect" class="collapse panel panel-default" style="margin-top: 20px">
+                    <div class="panel-heading">INSTUCTIONS</div>
+                    <form action="" class="panel-body">
+                        <p id="bid-instruction">Enter your bid amount. Bid interval for this item is <span class="nowrap">Php 5.00</span>. The minimum input bid is currently at <span class="nowrap">Php 280.00</span>. <a href="#">Learn more.</a></p>
+                        <div class="input-group">
+                            <label for="bid-amount" class="input-group-addon">Your bid</label>
+                            <input id="bid-amount" type="number" class="form-control" name="bid-amount" placeholder="Please enter your bid" step="5" min="280" value="280" required>
+                        </div>
+                        <div style="padding: 10px; text-align: center">
+                            <button type="submit" class="btn btn-success">Bid</button>
+                            <button type="button" id="bid-cancel" class="btn btn-default" data-toggle="collapse" data-target="#bid-sect">Cancel</button>
+                        </div>
+                    </form>
+                </div>
+                <div id="description">
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam est inventore quam culpa. Optio asperiores ex sint cum voluptate quidem ipsam, enim reiciendis eaque dolorem dolore aspernatur voluptatibus, eum quaerat!
+                        Voluptas odit numquam deserunt iure commodi libero ut omnis iusto optio possimus laborum labore quae, dolorum nulla culpa repellendus beatae eos molestias eveniet quidem consequuntur quis aut tempora. Qui, labore.
+                        Distinctio facilis, sequi provident in voluptates, rerum tempora, mollitia dolore maiores tenetur at ex fugiat similique officia vero minima nulla accusamus sed magni ducimus voluptatum possimus eos. Commodi, numquam vitae.
+                        Sequi, mollitia eum laborum necessitatibus omnis ducimus nam, nihil doloremque sit inventore et ipsum numquam incidunt rerum. Eaque repellat iste, id, veritatis quae itaque, inventore libero commodi sed fugiat error.
+                        Tenetur numquam assumenda vero sunt, quam adipisci porro ipsa doloremque corrupti necessitatibus veniam, fugiat vitae voluptate tempore mollitia dolores molestias impedit quaerat dolorem architecto? Magnam libero beatae provident harum corporis.
+                    </p>
                 </div>
             </div>
         </div>
-        <div class="col-sm-3">Related Items</div>
+        <div class="row">
+            <div class="col-sm-6">
+                <div id="details">
+                    <table class="table table-striped table-bordered">
+                        <tbody>
+                            <tr>
+                                <th>Seller</th>
+                                <td>John Doe</td>
+                            </tr>
+                            <tr>
+                                <th>Initial price</th>
+                                <td>Php 175.00</td>
+                            </tr>
+                            <tr>
+                                <th>Upload date</th>
+                                <td>January 1, 1970</td>
+                            </tr>
+                            <tr>
+                                <th>View count</th>
+                                <td>528 times</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div id="history">
+                    <div class="timeline-card">
+                        <table>
+                            <tr>
+                                <td><div class="bid-date">June 12, 2018</div></td>
+                                <td><div class="end-sprite"></div></td>
+                                <td><div class="bid-amount">Php 275.00</div></td>
+                                <td><div class="bidder"><a href="#">c-collamar</a></div></td>
+                            </tr>
+                            <tr>
+                                <td><div class="bid-date">June 10, 2018</div></td>
+                                <td><div class="connector-sprite"></div></td>
+                                <td><div class="bid-amount">Php 240.00</div></td>
+                                <td><div class="bidder"><a href="#">AustinZuniga</a></div></td>
+                            </tr>
+                            <tr>
+                                <td><div class="bid-date">December 27, 2017</div></td>
+                                <td><div class="start-sprite"></div></td>
+                                <td><div class="bid-amount">Php 206.00</div></td>
+                                <td><div class="bidder"><a href="#">userm83x</a></div></td>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
     <?php } else display404(); ?>
 
     <script src="http://localhost/augeo/global/vendor/jquery/dist/jquery.min.js"></script>
     <script src="http://localhost/augeo/global/vendor/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="js/view_item.js"></script>
 </body>
 </html>
