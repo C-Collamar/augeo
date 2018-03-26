18e42352abd50e161376a63625ecab768287cf37
C-Collamar <christian.collamar@gmail.com>
2018-03-27 02:01:01 +0800

    Automatically lock auction on time expiration

--- user/auctions/php/get_items_won.php
+++ user/auctions/php/get_items_won.php
@@ -12,6 +12,51 @@ if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQU
         exit();
     }
 
+    //get user bids to be automatically recognized as winning bid due to time limit
+    $query =
+    'SELECT '.
+        'GROUP_CONCAT(bid.bid_id) AS bid_ids, '.
+        'GROUP_CONCAT(bid.item_id) AS item_ids '.
+    'FROM '.
+        'augeo_application.bid, '.
+        'augeo_user_end.item '.
+    'WHERE '.
+        'bid.item_id = item.item_id AND '.
+        'item.state = 0 AND '.
+        'item.expiration_date < CURRENT_TIMESTAMP() AND '.
+        "$account_id_session = (".
+            'SELECT bid.user_id FROM augeo_application.bid ORDER BY bid.amount LIMIT 1'.
+        ')';
+    if(!$result = $conn->query($query)) {
+        header($_SERVER['SERVER_PROTOCOL']." 500 Internal Server Error");
+        exit(mysqli_error($conn).PHP_EOL.$query);
+    }
+    $row = $result->fetch_assoc();
+
+    if($row['item_ids'] && $row['bid_ids']) {
+        //set state of items to sold in selected bids above
+        $item_ids = $row['item_ids'];
+        $query = "UPDATE augeo_user_end.item SET state = 1 WHERE item.item_id IN ($item_ids)";
+        if(!$result = $conn->query($query)) {
+            header($_SERVER['SERVER_PROTOCOL']." 500 Internal Server Error");
+            exit(mysqli_error($conn).PHP_EOL.$query);
+        }
+        
+        //each bid ID will be used to make a deal
+        $bid_pool = explode(',', $row['bid_ids']);
+        $query = '';
+        foreach($bid_pool as $bid_id) {
+            $query .= "($bid_id, DATE_ADD(NOW(), INTERVAL 5 DAY)),";
+        }
+        $query =
+        'INSERT INTO augeo_application.deal (bid_id, due_date) VALUES '.substr($query, 0, -1);
+    
+        if(!$result = $conn->query($query)) {
+            header($_SERVER['SERVER_PROTOCOL']." 500 Internal Server Error");
+            exit(mysqli_error($conn).PHP_EOL.$query);
+        }
+    }
+
     $query =
     'SELECT '.
         'user.user_id, '.
@@ -34,11 +79,11 @@ if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQU
         'item.item_id = item_img.item_id AND '.
         'item.user_id = user.user_id '.
     'GROUP BY deal.deal_id '.
-'ORDER BY deal.timestamp DESC';
+    'ORDER BY deal.timestamp DESC';
 
     if(!$result = $conn->query($query)) {
         header($_SERVER['SERVER_PROTOCOL']." 500 Internal Server Error");
-        exit(mysqli_error($conn));
+        exit(mysqli_error($conn).PHP_EOL.$query);
     }
 
     $items_in_auction = [];
